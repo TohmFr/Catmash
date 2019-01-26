aaf750297a176e9ea9b3b5bdf14cd0ff031cbe89
TohmFr <thomas.bardy@gmail.com>
2019-01-26 14:48:10 +0100

    Vote action Without update

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -6,6 +6,8 @@ import { actionCreators } from '../store/Cats';
 
 
 class Home extends Component {
+
+
     componentDidMount() {
         // This method is called when the component is first added to the document
         this.ensureDataFetched();
@@ -13,36 +15,57 @@ class Home extends Component {
 
     componentDidUpdate() {
         // This method is called when the route parameters change
-       // this.ensureDataFetched();
+        // this.ensureDataFetched();
     }
 
     ensureDataFetched() {
         this.props.requestRandomCats();
     }
 
+
+
+    //Render
     render() {
         return (
             <div>
                 <h1>CatsVote</h1>
-                {renderCats(this.props)}
+                {this.renderCats(this.props)}
 
             </div>
         );
     }
+
+
+    //Render part
+    renderCats(props) {
+        const cats = props.cats;
+        return (
+            <div>
+                {cats.map(cat =>
+                    <div key={cat.id} onClick={(e) => this.vote(cat, cats, e)}>
+                        <img src={cat.urlImage} alt="cat pic" />
+                    </div>
+                )}
+            </div>
+        );
+    }
+
+
+    //Vote for a cat
+    vote(winner, cats, e) {
+        //winnig cat
+        const winnerId = winner.id;
+
+        //losing cat
+        const losingId = cats.find(c => c.id !== winnerId).id;
+
+        console.log(winnerId, losingId);
+
+    }
 }
 
 
-function renderCats(props) {
-    return (
-        <ul>
-            {props.cats.map(cat =>
-                <li key={cat.key}>
-                    <img src={cat.urlImage} alt="cat pic" />
-                </li>
-            )}
-        </ul>
-    );
-}
+
 
 export default connect(
     state => state.cats,
